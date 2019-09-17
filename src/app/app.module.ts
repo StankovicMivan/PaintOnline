73f120c640694a4196801732283375bb6f60206d
Ivan <stankovicmivan@gmail.com>
2019-09-17 14:48:10 +0200

    draw component impl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { DrawComponent } from './draw/draw.component';
+import {RouterModule} from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { DrawComponent } from './draw/draw.component';
     DrawComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {DrawComponent} from './draw/draw.component';
+
+
+const routes: Routes = [
+  { path: 'draw', component: DrawComponent }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ],
+  declarations: []
+})
+export class AppRoutingModule { }
