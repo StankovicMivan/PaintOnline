4874f081eeb5cfd574f38479d39242435c353eb0
Ivan <stankovicmivan@gmail.com>
2019-09-26 00:06:49 +0200

    done for today

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {
+  MatToolbarModule
+} from "@angular/material";
 
 import { AppComponent } from './app.component';
 import { DrawComponent } from './draw/draw.component';
@@ -8,20 +11,26 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app.routing.module';
 import {InputUserDataFormComponent} from './input-user-data-form/input-user-data-form.component';
 import {DisplayUserDataComponent} from './display-user-data/display-user-data.component';
+import {HeaderComponent} from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     DrawComponent,
     InputUserDataFormComponent,
-    DisplayUserDataComponent
+    DisplayUserDataComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    RouterModule
+    RouterModule,
+    MatToolbarModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
